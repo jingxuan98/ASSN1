ddec4f3be5b6317ae32703cb854cb13008ddfa9f
jingxuan98 <jingxuan.tmtn@gmail.com>
2019-11-03 10:49:14 +0800

    Left CSS and login if want

--- src/ASSN1/AlertBox.java
+++ src/ASSN1/AlertBox.java
@@ -7,9 +7,13 @@ import javafx.scene.control.*;
 import javafx.geometry.*;
 
 public class AlertBox {
+	
+	public static String cucibleID = "";
 
 	public static void display(String title) {
 		Stage window = new Stage();
+		
+		cucibleID = title;
 		
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle(title);
@@ -17,6 +21,88 @@ public class AlertBox {
 		
 		Label label = new Label();
 		label.setText("Are you sure that you want to select this place (" + title + ") ?");
+		GridPane.setConstraints(label,10,0);
+		
+		Button button1=new Button("Yes");
+		GridPane.setConstraints(button1,10,1);
+		Button button2=new Button("No");
+		GridPane.setConstraints(button2,10,2);
+		button1.setOnAction(e -> 
+			{
+			//main.writeFile();
+			main.guiStage.setScene(main.scene2) ;
+			window.close();});
+		
+		button2.setOnAction(e -> window.close());
+		
+		GridPane layout=new GridPane();
+		layout.setPadding(new Insets(10,10,10,10));
+		layout.setVgap(5);
+		layout.setHgap(10);
+		layout.getChildren().addAll(label,button1,button2);
+		Scene scene  =new Scene(layout,500,125);
+		
+		window.setScene(scene);
+		window.showAndWait();
+	}
+	
+	public static void displayOccupied(String title, String name, String matricNo, String date, String spvName) {
+		
+		Stage window = new Stage();
+		
+		cucibleID = title;
+		
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);
+		
+		Label label = new Label();
+		label.setText("This is occupied by the student with following information:");
+		GridPane.setConstraints(label,1,0);
+		
+		Label labelName = new Label();
+		labelName.setText("Name : " + name);
+		GridPane.setConstraints(labelName,1,4);
+		
+		Label labelMatricNo = new Label();
+		labelMatricNo.setText("MatricNo : " + matricNo);
+		GridPane.setConstraints(labelMatricNo,1,6);
+		
+		Label labelDate = new Label();
+		labelDate.setText("Check in Date : " + date);
+		GridPane.setConstraints(labelDate,1,8);
+		
+		Label labelSpvName = new Label();
+		labelSpvName.setText("Supervisor's Name : " + spvName);
+		GridPane.setConstraints(labelSpvName,1,10);
+		
+		GridPane layout=new GridPane();
+		layout.setPadding(new Insets(10,10,10,10));
+		layout.setVgap(5);
+		layout.setHgap(10);
+		
+		layout.getChildren().addAll(label, labelName, labelMatricNo, labelDate, labelSpvName);
+		
+		Scene scene  =new Scene(layout,500,200);
+		
+		window.setScene(scene);
+		window.showAndWait();
+	}
+	
+	public static void displayTest(String title, String name3) {
+		
+		if(name3 !=null) {
+		
+		Stage window = new Stage();
+		
+		cucibleID = title;
+		
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);
+		
+		Label label = new Label();
+		label.setText("Are you sure that you want to select this place (" + title + ") ? by " + name3);
 		
 		
 		GridPane.setConstraints(label,10,0);
@@ -36,6 +122,9 @@ public class AlertBox {
 		
 		window.setScene(scene);
 		window.showAndWait();
+		}else {
+			System.out.println("Problem Occured");
+		}
 	}
 	
 }
--- src/ASSN1/main.java
+++ src/ASSN1/main.java
@@ -21,15 +21,22 @@ import java.util.*;
 import java.io.*;
 
 public class main extends Application {
-
+			
+			private static Scanner sc;
 			private static TextField stdname,mtcnum,cid,spvname;
 			private static Button buttonC1;
+			private static ArrayList<String> cucibleIDBuffer = new ArrayList<String>();
+			private static String[] cucibleID = new String[100],name  = new String[100],matricNo  = new String[100],date  = new String[100],spvName  = new String[100];
+			public static Stage guiStage;
+			public static Scene scene1,scene2,scene3,scene4,scene5,scene6,sceneChoose;
+			
+			
 			Button button1,button2,button3,buttonN,buttonP;
-			Scene scene1,scene2,scene3,scene4,scene5,scene6;
 			Label label1,label2,label3,label4;
 			Label info1,info2,info3,info4,info5;
 			Label error1;
 			
+			//To store the file location as variable
 			private static File file;
 			
 			
@@ -39,6 +46,7 @@ public class main extends Application {
 					
 					file = new File("StudentData.txt");
 				
+					//Create a new file if doesn't exist
 					if(!file.exists()) {
 						file.createNewFile();
 					}
@@ -53,17 +61,17 @@ public class main extends Application {
 				openFile();
 				readFile();
 				launch(args);
-				//closeFile();
 			}
 			
 			
+			//Writing data to the text file
 			public static void writeFile() {
 				
 					try {
 						
 						PrintWriter writeFile = new PrintWriter(new BufferedWriter(new FileWriter(file,true)));
 						
-						writeFile.print(buttonC1.getText()+"\t"+ stdname.getText()+"\t"+mtcnum.getText()+"\t"+cid.getText()+
+						writeFile.print(AlertBox.cucibleID +"\t"+ stdname.getText()+"\t"+mtcnum.getText()+"\t"+cid.getText()+
 								"\t"+spvname.getText()+"\r\n");
 						
 						writeFile.close();
@@ -72,19 +80,32 @@ public class main extends Application {
 				}
 			}
 			
+			
+			//Reading data from the text file
 			public static void readFile() {
+
 				
 				try {
-					Scanner sc = new Scanner(file);
+					sc = new Scanner(file);
+					int i =0;
 					
 					while(sc.hasNext()) {
 						String readLine = sc.nextLine();
 						
 						String[] splitted = readLine.split("\t");
+							
+							cucibleID[i] = splitted[0];
+							name[i] = splitted[1];
+							matricNo[i] = splitted[2];
+							date[i] = splitted[3];
+							spvName[i] = splitted[4];
+							
+							System.out.println(cucibleID[i] +" "+ name[i]);
+							
+							i++;
+						}
 						
-						for(int i=0; i< splitted.length;i++)
-							System.out.println(splitted[i] + "-");
-					}
+					
 				}catch(Exception e) {
 						e.printStackTrace();
 					}
@@ -95,68 +116,102 @@ public class main extends Application {
 			
 			
 			@Override
-			public void start(Stage primaryStage) throws Exception{
+			public void start(Stage primaryStag) throws Exception{
+				
+				guiStage = primaryStag;
+				
 				//scene1 information column for student
-				primaryStage.setTitle("INFORMATION STORING CENTRE");
+				guiStage.setTitle("INFORMATION STORING CENTRE");
 				label1=new Label("Do you a student?");
 				GridPane.setConstraints(label1,10,0);
 				button1=new Button("Yes");
 				GridPane.setConstraints(button1,10,1);
 				button2=new Button("No");
 				GridPane.setConstraints(button2,10,2);
-				button1.setOnAction(e ->primaryStage.setScene(scene2));
-				button2.setOnAction(e ->primaryStage.setScene(scene3));
+				button1.setOnAction(e ->guiStage.setScene(sceneChoose));
+				button2.setOnAction(e ->guiStage.setScene(scene3));
 				GridPane layout=new GridPane();
 				layout.setPadding(new Insets(10,10,10,10));
 				layout.setVgap(5);
 				layout.setHgap(10);
 				layout.getChildren().addAll(label1,button1,button2);
 				scene1=new Scene(layout,350,120);
-				primaryStage.setScene(scene1);
-				primaryStage.show();
+				scene1.getStylesheets().add(getClass().getResource("application.css").toString());
+				guiStage.setScene(scene1);
+				guiStage.show();
 				
 				
 				
 				//Choose Floor Plan
-				
 				TabPane layoutChooseTab = new TabPane();
 				Tab tab1 = new Tab("1-Service Computing");
-				
+
 				GridPane gridChoose1=new GridPane();
 				gridChoose1.setPadding(new Insets(10,10,10,10));
 				gridChoose1.setVgap(8);
 				gridChoose1.setHgap(10);
 				
-				
 				for(int i = 1; i<=12; i++) {
-					
-//					char x = 'A';
+
 					int y= 65 + i-1;
 					char x = (char)y;
-					
+
 					for(int j = 1; j<=5; j++) {
 						
-						buttonC1=new Button( Integer.toString(1) + String.valueOf(x) + Integer.toString(j));
-						buttonC1.setMaxWidth(100);
-						//buttonC1.setOnAction(e ->primaryStage.setScene(scene2));
-						buttonC1.setOnAction(e -> AlertBox.display(buttonC1.getText()));
+						int z = 0;
+						boolean check = true;
+						String seatID = Integer.toString(1) + String.valueOf(x) + Integer.toString(j);
 						
-						GridPane.setConstraints(buttonC1,j+5,i+1);
-						
-						gridChoose1.getChildren().add(buttonC1);
-						
-					}
-					
-					;
-				}
-				
-				Button buttonCC=new Button("Check");
-				buttonCC.setOnAction(e -> writeFile());
-				GridPane.setConstraints(buttonCC,25,25);
-				
-				gridChoose1.getChildren().add(buttonCC);
+						while(cucibleID[z]!= null) {
+							
+							
+							if(seatID.equals(cucibleID[z])) {
+								
+								System.out.println("Occupied");
+								
+								Button buttonx=new Button("X");
+								buttonx.setMaxWidth(100);
+								
+								String name1 = name[z];
+								String matricNo1 = matricNo[z];
+								String date1 = date[z];
+								String spvName1 = spvName[z];
+								
+								buttonx.setOnAction(e -> 
+								{
+									
+								//Trigger the AlertBox for not available spaces	
+								AlertBox.displayOccupied(seatID, name1, matricNo1, date1, spvName1);
+								
+								});
+								
 
-				
+								GridPane.setConstraints(buttonx,j+5,i+1);
+
+								gridChoose1.getChildren().add(buttonx);
+								check = false;
+								break;
+							}
+							
+							z++;
+						}
+						
+						if(!check)
+							continue;
+
+						Button buttonC1=new Button(seatID);
+						buttonC1.setMaxWidth(100);
+						
+						//Trigger the Alert Box to let user book the place
+						buttonC1.setOnAction(e -> AlertBox.display(buttonC1.getText()));
+
+						GridPane.setConstraints(buttonC1,j+5,i+1);
+
+						gridChoose1.getChildren().add(buttonC1);
+
+					};
+				}
+
 				tab1.setContent(gridChoose1);
 				layoutChooseTab.getTabs().add(tab1);
 				
@@ -179,7 +234,7 @@ public class main extends Application {
 						
 						Button buttonC2=new Button( Integer.toString(1) + String.valueOf(x) + Integer.toString(j));
 						buttonC2.setMaxWidth(100);
-						buttonC2.setOnAction(e ->primaryStage.setScene(scene1));
+						buttonC2.setOnAction(e ->guiStage.setScene(scene1));
 						
 						GridPane.setConstraints(buttonC2,j+5,i+1);
 						
@@ -198,7 +253,7 @@ public class main extends Application {
 				
 				layoutChooseTab.getTabs().add(tab3);
 				
-				Scene sceneChoose = new Scene(layoutChooseTab,800,800);
+				sceneChoose = new Scene(layoutChooseTab,800,800);
 				
 				
 				
@@ -252,11 +307,19 @@ public class main extends Application {
 				
 				//buttonN.setOnAction(e ->primaryStage.setScene(scene4));
 				
-				buttonN.setOnAction(e ->primaryStage.setScene(sceneChoose));
+				buttonN.setOnAction(e ->
+					{writeFile();
+					//cucibleIDBuffer.add(AlertBox.cucibleID);
+					guiStage.setScene(scene6);
+					stdname.clear();
+					mtcnum.clear();
+					cid.clear();
+					spvname.clear();
+					});
 				
 				GridPane.setConstraints(buttonN,1,6);
 				buttonP=new Button("Previous");
-				buttonP.setOnAction(e ->primaryStage.setScene(scene1));
+				buttonP.setOnAction(e ->guiStage.setScene(sceneChoose));
 				GridPane.setConstraints(buttonP,1,7);
 				
 				
@@ -274,10 +337,10 @@ public class main extends Application {
 				GridPane.setConstraints(error2,7,1);
 				button3=new Button("Return");
 				GridPane.setConstraints(button3,7,2);
-				button3.setOnAction(e ->primaryStage.setScene(scene1));
+				button3.setOnAction(e ->guiStage.setScene(scene1));
 				Button buttonE1= new Button("Exit");
 				GridPane.setConstraints(buttonE1,7,3);
-				buttonE1.setOnAction(e ->primaryStage.setScene(scene6));
+				buttonE1.setOnAction(e ->guiStage.setScene(scene6));
 				layout3.getChildren().addAll(error1,error2,button3,buttonE1);
 				scene3=new Scene(layout3,450,150);
 				
@@ -316,10 +379,10 @@ public class main extends Application {
 				GridPane.setConstraints(columntable, 1, 7);
 				
 				Button buttonN2=new Button("Next");
-	            buttonN2.setOnAction(e ->primaryStage.setScene(scene5));
+	            buttonN2.setOnAction(e ->guiStage.setScene(scene5));
 				GridPane.setConstraints(buttonN2,1,8);
 				Button buttonP2=new Button("Previous");
-				buttonP2.setOnAction(e ->primaryStage.setScene(scene2));
+				buttonP2.setOnAction(e ->guiStage.setScene(scene2));
 				GridPane.setConstraints(buttonP2,1,9);
 				
 				
@@ -332,31 +395,30 @@ public class main extends Application {
 				
 				Label label9=new Label("You have complete the information storing");
 				GridPane.setConstraints(label9,0,0);
-				Label label10=new Label("Click the Return button to go back the main menu");
+				Label label10=new Label("Click the Return button to go back the main menu for another entry");
 				GridPane.setConstraints(label10,0,1);
 				Label label11=new Label("Click the Exit button to end the process");
 				GridPane.setConstraints(label11,0,2);
 				Button buttonR=new Button("Return");
-				buttonR.setOnAction(e ->primaryStage.setScene(scene1));
+				buttonR.setOnAction(e ->guiStage.setScene(scene1));
 				GridPane.setConstraints(buttonR,0,3);
 				Button buttonE=new Button("Exit");
-				buttonE.setOnAction(e ->primaryStage.setScene(scene6));
+				buttonE.setOnAction(e ->{
+					sc.close();
+					guiStage.setScene(scene6);
+				});
 				GridPane.setConstraints(buttonE,0,4);
 				GridPane layout5=new GridPane();
 				layout5.setPadding(new Insets(10,10,10,10));
 				layout5.setVgap(8);
 				layout5.setHgap(10);
 				layout5.getChildren().addAll(label9,label10,label11,buttonR,buttonE);
-				scene5=new Scene(layout5,400,200);
+				scene5=new Scene(layout5,500,200);
 				
-				Label label12=new Label("Programme exit.Thanks for cooperation");
+				Label label12=new Label("Your process is complete .Thanks for cooperation");
 				StackPane layout6=new StackPane();
 				layout6.getChildren().add(label12);
 				scene6=new Scene(layout6,400,200);
-				
-				
-				
-				
 				
 				
 				}
